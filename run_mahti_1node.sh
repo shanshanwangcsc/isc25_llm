ae711d8e33106fefb79348775a856dfc46e95d4d
Mats Sjöberg <mats.sjoberg@csc.fi>
2025-04-29 13:07:17 +0300

    Changed to correct project, using reservation

--- run_mahti_1node.sh
+++ run_mahti_1node.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#SBATCH --account=dac
+#SBATCH --account=project_2012524
 #SBATCH --partition=gputest
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=128
 #SBATCH --time=0:15:00
 #SBATCH --gres=gpu:a100:4
+#SBATCH --reservation=scc-gpu
+
 module purge
 module load pytorch/2.5
 
 
-export HF_TOKEN="your token"
-export HF_HOME="/scratch/dac/${USER}/hf_cache"
+#export HF_TOKEN="your token"
+export HF_HOME="/scratch/project_2012524/${USER}/hf-cache"
 mkdir -p $HF_HOME
 
 
@@ -22,10 +24,10 @@ if [ -z "${HF_TOKEN}" ]; then
 fi
 
 # Speed benchmark
-#torchrun --nproc_per_node=4 main.py --benchmark speed --device-type cuda
+torchrun --nproc_per_node=4 main.py --benchmark speed --device-type cuda
 
 # Speed benchmark and save stdout/loggings
-srun torchrun --nproc_per_node=4 main.py --benchmark speed --device-type cuda > loggings/speed.log 2<&1
+#srun torchrun --nproc_per_node=4 main.py --benchmark speed --device-type cuda > loggings/speed.log 2<&1
 
 
 # Speed and accuracy benchmark
